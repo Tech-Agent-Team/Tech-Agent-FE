a9002d640965423220f9d6f9a9c2e1daa00f3733
sajedaalrababah <sajedaalrababah@icloud.com>
2023-08-31 23:36:57 +0300

    s

--- tech-agent/components/baseheader.js
+++ tech-agent/components/baseheader.js
@@ -5,8 +5,8 @@ import { useAuth } from '@/context/auth';
 const Header = () => {
   const { logout } = useAuth(); 
   return (
-    <header className="bg-gray-900">
-      <nav className="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
+    <header className="bg-gray-900 sticky top-0 z-50">
+      <nav className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <ul className="flex items-center h-16">
           <li>
             <Link href="/">
@@ -15,13 +15,18 @@ const Header = () => {
           </li>
 
           <li className="ml-auto">
-            <Link href="/LoginPage">
-              <button className="text-white cursor-pointer" onClick={""}>About</button>
+            <Link href="/AcceptedOrder">
+              <button className="text-white cursor-pointer mr-2">Accepted Orders</button>
             </Link>
           </li>
           <li className="ml-auto">
             <Link href="/LoginPage">
-              <button className="text-white cursor-pointer">Login</button>
+              <button className="text-white cursor-pointer mr-2">About</button>
+            </Link>
+          </li>
+          <li className="ml-auto">
+            <Link href="/LoginPage">
+              <button className="text-white cursor-pointer mr-2">Login</button>
             </Link>
           </li>
           <li className="ml-auto">
--- tech-agent/pages/CustomerOrder.js
+++ tech-agent/pages/CustomerOrder.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from '../components/HeaderUser';
 import useResource from '@/Hooks/useResource2';
 const customerOrder = () => {
   const { response, deleteResource } = useResource();
+  
   const handleDeleteOrder = (orderId) => {
     deleteResource(orderId);
+  
   };
+  
+ 
   return (
     <div>
       <Header />
@@ -36,11 +41,11 @@ const customerOrder = () => {
       <div className="mt-8 bg-red">
         <div className="pb-2 mb-8 border-b">Panding order</div> {/* Category title */}
         <div className="flex flex-wrap gap-5">
-          {response.map(order => (
-            !order.state_is_ongoing && (
-              <div key={order.id} className="w-1/3 p-4 border">
-                {/* Card content */}
-                <img src={order.image} alt={order.description} className="w-full mb-2" />
+        {response.map(order => (
+  !order.state_is_ongoing && (
+    <div key={order.id} className="w-1/3 p-4 border">
+      {/* Card content */}
+      <img src={order.image} alt={order.description} className="w-full mb-2" />
                 <h3 className="text-lg font-semibold">{order.description}</h3>
                 <p>Technician Type: {order.technician_type}</p>
                 <p>Address: {order.address}</p>
@@ -48,13 +53,16 @@ const customerOrder = () => {
                 {order.eta_arrival_time && (
                   <p>Estimated Arrival Time: {order.eta_arrival_time}</p>
                 )}
-                <div className="flex justify-between mt-4">
-                <button onClick={() => handleDeleteOrder(order.id)} className="px-4 py-2 text-white bg-red-500 rounded-md">Delete</button>
-                  <button onClick={() => handleUpdateOrder(order.id)} className="px-4 py-2 text-white bg-blue-500 rounded-md">Update</button>
-                </div>
-              </div>
-            )
-          ))}
+      {/* ... (existing code) */}
+      <div className="flex justify-between mt-4">
+        <button onClick={() => handleDeleteOrder(order.id)} className="px-4 py-2 text-white bg-red-500 rounded-md">Delete</button>
+        <button onClick={() => handleUpdateOrder(order.id)} className="px-4 py-2 text-white bg-green-500 rounded-md">Update
+       </button>
+       
+      </div>
+    </div>
+  )
+))}
         </div>
       </div>
     </div>
