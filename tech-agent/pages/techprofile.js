a61986e85f3fcda5e8759e2f7dbeac044982fe76
Malik-Essa99 <malikhodrob99@gmail.com>
2023-09-02 15:50:42 +0000

    added update profile for both tech and customer

--- tech-agent/pages/techprofile.js
+++ tech-agent/pages/techprofile.js
@@ -3,47 +3,78 @@ import useResource from '@/Hooks/useResource';
 import { useAuth } from '@/context/auth';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router'; // Import the useRouter hook
+import Link from 'next/link'; // Import Link
 
 const TechProfile = () => {
-    const { user } = useAuth();
-    const urlenv = process.env.NEXT_PUBLIC_URL;
-    const url = `${urlenv}/api/technician/profile/${user.username}/`;
-    const { response: data1, error: error1 } = useResource(url);
-    const router = useRouter(); // Initialize the router object
-    useEffect(() => {
-      // Check if the user is authenticated and their role
-      if (user) {
-        if (!user.is_technician) {
-          router.push('./userprofile'); // Redirect to the technician's home
-        } 
-      }
-      else {
-        router.push('../'); 
-      }
-    }, [user, router]);
+  const { user } = useAuth();
+  const urlenv = process.env.NEXT_PUBLIC_URL;
+  const url = `${urlenv}/api/technician/profile/${user.username}/`;
+  const { response: data1, error: error1 } = useResource(url);
+  const router = useRouter(); // Initialize the router object
 
-    if (user && user.is_technician) {    
-    return (
-      <div className="min-h-screen bg-gray-100">
-        <Header />
-  
-        <div className="max-w-2xl p-6 mx-auto">
-          {data1 && data1.user && (
-            <div className="p-6 bg-white rounded-lg shadow-lg">
-              <img src={data1.image} alt="User Avatar" className="w-32 h-32 mx-auto mb-4 rounded-full" />
-              <h1 className="mb-2 text-2xl font-semibold text-center">{data1.user.username}</h1>
-              <p className="mb-4 text-center text-gray-600">Profession: {data1.profession}</p>
-              <p className="mb-4 text-gray-600">Description: {data1.description}</p>
-              <p className="text-gray-600">Average Rating: {data1.average_rating}</p>
-            </div>
-          )}
-  
-          {error1 && (
-            <div className="text-red-500">Error loading profile data: {error1.message}</div>
-          )}
-        </div>
+  useEffect(() => {
+    // Check if the user is authenticated and their role
+    if (user) {
+      if (!user.is_technician) {
+        router.push('./userprofile'); // Redirect to the technician's home
+      }
+    } else {
+      router.push('../');
+    }
+  }, [user, router]);
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      {/* {console.log(data1,user.username)} */}
+      <div>
+        {data1?.user?.username === user.username ? (
+          <div className="max-w-2xl p-6 mx-auto">
+            {data1 && data1.user && (
+              <div className="p-6 bg-white rounded-lg shadow-lg">
+                <img
+                  src={data1.image}
+                  alt="User Avatar"
+                  className="w-32 h-32 mx-auto mb-4 rounded-full"
+                />
+                <h1 className="mb-2 text-2xl font-semibold text-center">{data1.user.username}</h1>
+                <p className="mb-4 text-center text-gray-600">Profession: {data1.profession}</p>
+                <p className="mb-4 text-gray-600">Description: {data1.description}</p>
+                <p className="text-gray-600">Average Rating: {data1.average_rating}</p>
+                <Link href="/UpdateTechnicianProfile">
+                  <button className="text-black cursor-pointer">Edit Profile</button>
+                </Link>
+              </div>
+            )}
+
+            {error1 && (
+              <div className="text-red-500">Error loading profile data: {error1.message}</div>
+            )}
+          </div>
+        ) : (
+          <div className="max-w-2xl p-6 mx-auto">
+            {data1 && data1.user && (
+              <div className="p-6 bg-white rounded-lg shadow-lg">
+                <img
+                  src={data1.image}
+                  alt="User Avatar"
+                  className="w-32 h-32 mx-auto mb-4 rounded-full"
+                />
+                <h1 className="mb-2 text-2xl font-semibold text-center">{data1.user.username}</h1>
+                <p className="mb-4 text-center text-gray-600">Profession: {data1.profession}</p>
+                <p className="mb-4 text-gray-600">Description: {data1.description}</p>
+                <p className="text-gray-600">Average Rating: {data1.average_rating}</p>
+              </div>
+            )}
+
+            {error1 && (
+              <div className="text-red-500">Error loading profile data: {error1.message}</div>
+            )}
+          </div>
+        )}
       </div>
-    );
-  };};
-  
-  export default TechProfile;
+    </div>
+  );
+};
+
+export default TechProfile;
--- tech-agent/pages/userprofile.js
+++ tech-agent/pages/userprofile.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 
 import { useRouter } from 'next/router'; // Import the useRouter hook
-
+import Link from 'next/link';
 import Header from '../components/Header';
 import useResource from '@/Hooks/useResource';
 import { useAuth } from '@/context/auth';
+
 
 const UserProfile = () => {
   const { user } = useAuth();
@@ -19,30 +20,53 @@ const UserProfile = () => {
     if (user) {
       if (user.is_technician) {
         router.push('./techprofile'); // Redirect to the technician's home
-      } 
+      }
     }
     else {
-      router.push('../'); 
+      router.push('../');
     }
   }, [user, router]);
-
-  if (user && !user.is_technician) {
+  // if ()
+  if (user) {
     return (
       <div className="min-h-screen bg-gray-100">
         <Header />
+        {console.log(data1.username,user.username)}
+        <div>
+          {
+            data1?.username === user.username ? (
+              <div className="max-w-2xl p-6 mx-auto">
+                {data1 && (
+                  <div className="p-6 bg-white rounded-lg shadow-lg">
+                    <h1 className="mb-2 text-2xl font-semibold text-center">{data1.username}</h1>
+                    <p className="mb-4 text-center text-gray-600">Email: {data1.email}</p>
+                    <p className="mb-4 text-center text-gray-600">Phone: {data1.phone}</p>
+                    <p className="mb-4 text-center text-gray-600">Location: {data1.location}</p>
+                    <p className="text-center text-gray-600">Number of Orders: {data1.num_orders}</p>
+                    <Link href="/UpdateCustomerProfile">
+                      <button className="text-black cursor-pointer">Edit Profie</button>
+                    </Link>
 
-        <div className="max-w-2xl p-6 mx-auto">
-          {data1 && (
-            <div className="p-6 bg-white rounded-lg shadow-lg">
-              <h1 className="mb-2 text-2xl font-semibold text-center">{data1.username}</h1>
-              <p className="mb-4 text-center text-gray-600">Email: {data1.email}</p>
-              <p className="mb-4 text-center text-gray-600">Phone: {data1.phone}</p>
-              <p className="mb-4 text-center text-gray-600">Location: {data1.location}</p>
-              <p className="mb-4 text-center text-gray-600">Is Customer: {data1.is_customer ? "Yes" : "No"}</p>
-              <p className="text-center text-gray-600">Number of Orders: {data1.num_orders}</p>
-            </div>
-          )}
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className="max-w-2xl p-6 mx-auto">
+                {data1 && (
+                  <div className="p-6 bg-white rounded-lg shadow-lg">
+                    <h1 className="mb-2 text-2xl font-semibold text-center">{data1.username}</h1>
+                    <p className="mb-4 text-center text-gray-600">Email: {data1.email}</p>
+                    <p className="mb-4 text-center text-gray-600">Phone: {data1.phone}</p>
+                    <p className="mb-4 text-center text-gray-600">Location: {data1.location}</p>
+                    <p className="mb-4 text-center text-gray-600">Is Customer: {data1.is_customer ? "Yes" : "No"}</p>
+                    <p className="text-center text-gray-600">Number of Orders: {data1.num_orders}</p>
+                  </div>
+                )}
+              </div>
+            )
+          }
         </div>
+
       </div>
     );
   } else {
