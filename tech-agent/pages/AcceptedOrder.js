41219f030aa1eb6b638fb9b1f1c89236aa037b71
BayanBanat <bayankbanat99@gmail.com>
2023-09-05 20:48:46 +0000

    bayan-v-10

--- tech-agent/pages/AcceptedOrder.js
+++ tech-agent/pages/AcceptedOrder.js
@@ -4,6 +4,12 @@ import { useAuth } from "@/context/auth";
 import useResource from "@/Hooks/useResource";
 import { useRouter } from "next/router";
 import styles from '../styles/accept.module.css';
+import Footer from "@/components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faPaperPlane, faCancel } from "@fortawesome/free-solid-svg-icons";
+;
+
 
 import Cookies from "js-cookie"; // Import Cookies
 const acceptedorder = () => {
@@ -71,71 +77,86 @@ const acceptedorder = () => {
         >
 
       
-        <div className={`flex gap-20 ${styles.profileContainer}`} style={{
-          padding: '90px', // Add padding to create space around the inner content
-          borderRadius:'50%'
+        <div className={`flex gap-20  ${styles.profileContainer}`} style={{
+          paddingTop:'70px', // Add padding to create space around the inner content
+          // borderRadius:'50%'
         }}>
           <Header />
           <div className={`flex flex-wrap justify-center items-center`}>
             {data1.map((order) => (
               <div key={order.id} className={` ${styles.flipcardcontainer}`}>
-                <div className={` ${styles.flipcard}`}>
-                  <div className={` ${styles.cardfront}`} style={{display:'flex', flexDirection:'column',alignItems:'center'}}>
+                <div className={` ${styles.flipcard} `}>
+                  <div className={` ${styles.cardfront}`} style={{display:'block', flexDirection:'column',alignItems:'flex-start',}}>
+                   <div  >
                     <img
                       src={imageurl + order.image}
                       alt={order.description}
-                      className={`w-full mb-2 `}
+                      className={`${styles.fixedimagesize}`}
+                      
                     />
-                    <h3 className="text-lg font-semibold">{order.description}</h3>
-                    <p>Go to customer profile : {""}</p>
-                    <span
-                      onClick={customerviewhandelr}
-                      data-namecustomer={order.customer_name.username}
-                    >
-                      {order.customer_name.username}
-                    </span>
-                 
-              {/* Render comments */}
-              <p>Technician Type: {order.technician_type}</p>
+                    </div>
+                    <div style={{padding:'10px'}}>
+                    <h3 className="text-lg font-bold text-white ">{order.description} </h3>
+                   
+              <p className="text-lg text-white font-smibold-white " >Technician Type: {order.technician_type}</p>
                   {order.eta_arrival_time && (
-                    <p>Estimated Arrival Time: {order.eta_arrival_time}</p>
+                    <p className="text-lg text-white font-smibold-white " >Estimated Arrival Time:  {order.eta_arrival_time}</p>
                   )}
+                  </div>
                   </div>
                   
                   <div className={` ${styles.cardback}`} style={{display:'flex', flexDirection:'column',alignItems:'center'}}>
-                    <div >
+                   
+                   <div>
+                <button onClick={() => handleCancel(order.id)} className="px-4 py-2 text-white bg-red-500 rounded-md"><FontAwesomeIcon icon={faCancel} className="text-2xl cursor-pointer hover:text-yellow-600" /></button>
+              </div>
+                   <div   >
+                    <p style={{padding:''}}>Go to customer profile : {""}</p>
+                    <span
+                      onClick={customerviewhandelr}
+                      data-namecustomer={order.customer_name.username}
+                      className="text-blue-500 underline cursor-pointer"
+                    >
+                      {order.customer_name.username}
+                    </span>
+
+                    <div className={` ${styles.messagebox}`}style={{ height: "150px", overflowY: "scroll" ,backgroundColor:'whitesmoke',borderRadius:'5px'}}>
                       {order.comments &&
                         order.comments.map((comment) => (
-                          <div key={comment.id}>
-                            <h3 className="text-lg font-semibold">{`${comment.sender}: ${comment.body}`}</h3>
+                          <div key={comment.id} >
+                            <h3 className="p-2 text-lg">{`${comment.sender}: ${comment.body}`}</h3>
                             {/* Render other comment properties as needed */}
                           </div>
                         ))}
                     </div>
+
+                    
                     <form onSubmit={(event) => handleSubmit(event, order.id)}>
                       <input
                         type="text"
                         placeholder="Add your message"
                         className="w-full p-2 mt-2 border rounded-md"
                         name="message"
+                        required
                       />
                       <button
                         className="px-4 py-2 mt-2 text-white bg-blue-500 rounded-md"
                         type="submit"
+                        
                       >
-                        Send
+                        <FontAwesomeIcon icon={faPaperPlane} className="text-2xl cursor-pointer hover:text-yellow-600" />
                       </button>
                      
                     </form>
-                    <div>
-                <button onClick={() => handleCancel(order.id)} className="px-4 py-2 text-white bg-red-500 rounded-md">cancel Order</button>
-              </div>
+                    </div>
+                    
                   </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
+        <Footer></Footer>
         </div>
       );
       
